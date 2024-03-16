ef03d5b693664061ff6255bb872bce5e06c28a79
cpg-qe <142965448+cpg-qe@users.noreply.github.com>
2024-03-16 11:53:05 +0530

    Create firewall.tf

---
+++ GCPSSH/GCPCompute/firewall.tf
@@ -0,0 +1,12 @@
+esource "google_compute_firewall" "webserver_ssh" {
+  name    = "webserver-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = ["webserver-instance"]
+  source_ranges = ["0.0.0.0/0"]
+}
