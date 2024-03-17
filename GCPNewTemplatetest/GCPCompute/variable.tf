23333d30da2ca89d982042a7669a4f90238ce169
cpg-qe <142965448+cpg-qe@users.noreply.github.com>
2024-03-17 16:25:57 +0530

    Create variable.tf

---
+++ GCPNewTemplatetest/GCPCompute/variable.tf
@@ -0,0 +1,36 @@
+variable "credentials" {}
+
+
+variable "ssh_public_key_path" {
+  description = "ssshkeyforgoogle.pub"
+  type        = string
+}
+
+
+variable "existing_network_name" {
+  default = "default"
+}
+
+variable "region" {
+  default = "us-east1"
+}
+
+variable "firewallname" {
+default = "itomlab-firewall"
+}
+
+variable "zone" {
+  default = "us-east1-b"
+}
+
+variable "hostname" {
+ default = "cpglabhost16"
+}
+
+variable "machinetype"{
+default = "f1-micro"
+}
+
+
+
+variable "project" {}
